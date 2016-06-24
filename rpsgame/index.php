613ba11527b0e70bcd3c7c6b34ef82d803e30c73
Michelle Cali <mctexx@gmail.com>
2016-06-24 12:18:12 -0500

    beginnings of game

---
+++ rpsgame/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>
+			rock | paper | scissors
+		</title>
+		<link href='https://fonts.googleapis.com/css?family=Lobster+Two:400,700,400italic,700italic|Arimo:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Cantata+One|Imprima' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="styles.css">	
+		<?php $current = 'home'; ?>
+	</head>
+	<body>
+
+<form action="action_page.php">
+  <input type="radio" name="choice" value="1" checked> rock<br>
+  <input type="radio" name="choice" value="2"> scissors<br>
+  <input type="radio" name="choice" value="3"> paper<br><br>
+  <input type="submit">
+</form>
+
+
+<!-- use generator to pick number 0,1,2; use number to find index in array? -->
+
+	</body>
