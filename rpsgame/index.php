a8c9b6e5a3e9b677c604d559f4c715b2443bd519
Michelle Cali <mctexx@gmail.com>
2016-06-24 15:38:38 -0500

    Ties work

--- rpsgame/index.php
+++ rpsgame/index.php
@@ -23,6 +23,9 @@
 			$computer_answer = rand(6,8);
 		  	$array = array ('6' => 'rock', '7' => 'paper', '8' => 'scissors');
 		  	echo "You chose " . $array[$player_answer]; echo " and I chose " . $array[$computer_answer];
+		  	if ($player_answer == $computer_answer) {
+		  		echo "<br> WE TIED! Play again?";
+		  	}
 		  }
 		?>
 	</body>
