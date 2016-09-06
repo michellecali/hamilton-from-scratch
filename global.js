249d1590be22f8c87565e3de02737ed8cf9f0fcf
Michelle Cali <mctexx@gmail.com>
2016-09-06 10:01:53 -0500

    one navBar animation working but janky

--- global.js
+++ global.js
@@ -7,17 +7,17 @@ window.addEventListener("load", function() {
 	var btn = document.getElementById("individualsButton");
 
 	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
+	var span = document.getElementById("individuals");
 
 	// When the user clicks the button, open the modal
 	btn.addEventListener("mouseover", function() {
-	    modal.style.display = "block";
+	    modal.style.display = "flex";
+	    span.innerHTML = "click to learn more";
 	});
 
 	// When the user clicks anywhere outside of the modal, close it
 	btn.addEventListener("mouseleave", function(event) {
-	    modal.style.display = "none";
-	    
+	    modal.style.display = "none"; 
+	    span.innerHTML = "<i class='fa fa-plus-circle' aria-hidden='true'></i>&nbsp;Individuals";  
 	});
 });
